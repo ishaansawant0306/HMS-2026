d8232bd3d61ed24c1c2a6c20e65ed313851ca499
ishaansawant0306 <24f2007256@ds.study.iitm.ac.in>
2026-04-09 17:59:38 +0530

    extras

--- start-dev.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem Start backend and frontend servers from the repo root.
-cd /d "%~dp0Backend"
-start "Backend" cmd /k "cd /d "%~dp0Backend" && python app.py"
-start "Frontend" cmd /k "cd /d "%~dp0Frontend" && npm install && npm run dev"
-
-ntimeout /t 3 /nobreak > nul
-nstart "" "http://localhost:3000"
